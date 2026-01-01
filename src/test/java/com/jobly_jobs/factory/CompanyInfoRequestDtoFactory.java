a0cd5bd80e6a1b89ead60e0ee27a059d863cc5b9
martijnvancitteren <martijnvancitteren@live.nl>
2026-01-01 18:15:18 +0100

    Add unit tests for CompanyInfoPromptGenerator and create request DTO factory

---
+++ src/test/java/com/jobly_jobs/factory/CompanyInfoRequestDtoFactory.java
@@ -0,0 +1,26 @@
+package com.jobly_jobs.factory;
+
+import com.jobly_jobs.domain.dto.request.CompanyInfoRequestDto;
+import com.jobly_jobs.domain.enums.Country;
+
+public class CompanyInfoRequestDtoFactory {
+
+    public static CompanyInfoRequestDto createCompanyInfoRequestDto() {
+        return new CompanyInfoRequestDto(
+                "Tech Solutions BV",
+                "www.techsolutions.com",
+                Country.THE_NETHERLANDS,
+                "www.techsolutions.com/careers/java-developer"
+        );
+    }
+
+    public static CompanyInfoRequestDto createCompanyInfoRequestDtoWithoutVacancyUrl() {
+        return new CompanyInfoRequestDto(
+                "Tech Solutions BV",
+                "www.techsolutions.com",
+                Country.THE_NETHERLANDS,
+                null
+        );
+    }
+}
+
