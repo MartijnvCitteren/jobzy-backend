40dfef20908f164a07af14676c9aaad9f1a52a10
martijnvancitteren <martijnvancitteren@live.nl>
2026-09-04 11:02:51 +0200

    fixes

--- jobzy-api/src/main/java/app/jobzy/api/adapter/in/rest/vacancy/mapper/response/VacancyResponseMapper.java
+++ jobzy-api/src/main/java/app/jobzy/api/adapter/in/rest/vacancy/mapper/response/VacancyResponseMapper.java
@@ -1,5 +1,6 @@
 package app.jobzy.api.adapter.in.rest.vacancy.mapper.response;
 
+import app.jobzy.api.adapter.in.rest.vacancy.mapper.HoursPerWeekMapper;
 import app.jobzy.api.domain.vacancy.Vacancy;
 import app.jobzy.api.vacancy.adapter.in.web.contract.VacancyResponse;
 import org.mapstruct.Mapper;
@@ -8,6 +9,7 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(
     componentModel = "spring",
+    uses = {HoursPerWeekMapper.class},
     unmappedSourcePolicy = ReportingPolicy.IGNORE,
     unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface VacancyResponseMapper {
